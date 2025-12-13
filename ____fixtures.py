36ca1431eb53ae510248a5d7b7bcac1951d890eb
Nikolai Kochelev <kochelev@yandex.ru>
2025-12-13 17:26:38 +0300

    Prune examples

--- ____fixtures.py
+++
@@ -1,5 +0,0 @@
-import pytest
-
-@pytest.fixture()
-def fixture_from_another_file() -> int:
-    return 10
--- algorithms/sorting.py
+++
@@ -1,3 +0,0 @@
-
-def do_python_list_sort(L: list[int]) -> list[int]:
-    return sorted(list)
